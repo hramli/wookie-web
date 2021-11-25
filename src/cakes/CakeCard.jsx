92addcaf3180098044520240b5cf4a80f8741be1
hramli <ramliharry48@yahoo.com>
2021-11-25 13:27:51 -0800

    moved CakeCard into its own component

---
+++ src/cakes/CakeCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export class CakeCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="cake-card col-lg-4">
+          <Link to={`/cake/${this.props.cakeId}`}>
+            <div className="cake-card-img-wrapper">
+              <img src={`${process.env.PUBLIC_URL}/images/cakes/${this.props.image}`} alt="" className="cake-card-img"/>
+            </div>
+          </Link>
+          <div className="cake-card-info-wrapper">
+            <Link to={`/cake/${this.props.cakeId}`}>
+              <span className="cake-card-title">{this.props.name}</span>
+            </Link>
+            <p className="cake-card-price">SGD {this.props.price.sixInch} - SGD {this.props.price.eightInch}</p>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
--- src/cakes/Cakes.jsx
+++ src/cakes/Cakes.jsx
@@ -1,29 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { CakeCard } from './CakeCard';
 import { WookieCakes } from '../shared/constants';
 import './CakeCard.css'
-
-export class CakeCard extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="cake-card col-lg-4">
-          <Link to={`/cake/${this.props.cakeId}`}>
-            <div className="cake-card-img-wrapper">
-              <img src={`${process.env.PUBLIC_URL}/images/cakes/${this.props.image}`} alt="" className="cake-card-img"/>
-            </div>
-          </Link>
-          <div className="cake-card-info-wrapper">
-            <Link to={`/cake/${this.props.cakeId}`}>
-              <span className="cake-card-title">{this.props.name}</span>
-            </Link>
-            <p className="cake-card-price">SGD {this.props.price.sixInch} - SGD {this.props.price.eightInch}</p>
-          </div>
-        </div>
-      </>
-    )
-  }
-}
 
 export function Cakes() {
   let cakeCards = []
